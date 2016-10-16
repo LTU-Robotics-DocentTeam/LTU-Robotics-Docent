b65e952dbcd3cb957bf23e7e103c2a0229832784
Luisx09 <lrodrigue@ltu.edu>
2016-10-16 14:52:29 -0400

    First Commit
    Added MVVM test program as a base

---
+++ RoboticDocentAssistant/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using MVVMTest.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            MVVMTest.ViewModel.ViewModel ViewModelObject = new MVVMTest.ViewModel.ViewModel();
+
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            ViewModelObject.LoadStudents(students);
+            ViewControl.DataContext = ViewModelObject;
+
+
+        }
+    }
+}
---
+++ RoboticDocentAssistant/ViewModel/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMTest.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMTest.ViewModel
+{
+    public class ViewModel
+    {
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents(ObservableCollection<Student> thingie)
+        {
+            /*
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+            */
+
+            Students = thingie;
+        }
+    } 
+}
