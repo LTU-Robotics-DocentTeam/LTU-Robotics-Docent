0ce1e5589581012b6741e8d22e1e3b80ae38cf89
Luisx09 <lrodrigue@ltu.edu>
2016-11-20 20:57:27 -0500

    Created Specific Sensor Modules
    Created SensorModule classes for each sensor. We might be able to keep
    sensor data processing to fewer modules than this, but it's a reasonable
    start

---
+++ RoboticDocentAssistant/HENRY/Modules/Sensors/HallEffectSensorModule.cs
@@ -0,0 +1,25 @@
+﻿using HENRY.ModuleSystem;
+using System.Timers;
+
+
+namespace HENRY.Modules.Sensors
+{
+    class HallEffectSensorModule : GenericSensorModule
+    {
+        //Timer t;
+        
+        public HallEffectSensorModule()
+        {
+            SetPropertyValue("ArraySensor1", false);
+            SetPropertyValue("ArraySensor2", false);
+            SetPropertyValue("ArraySensor3", false);
+            SetPropertyValue("ArraySensor4", false);
+            SetPropertyValue("ArraySensor5", false);
+        }
+
+        public override string GetModuleName()
+        {
+            return "HallEffectSensorModule";
+        }
+    }
+}
