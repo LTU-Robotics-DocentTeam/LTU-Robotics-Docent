17fee089d7502c8957d969aa9090806b2cb22f2b
Luisx09 <lrodrigue@ltu.edu>
2017-03-29 18:56:09 -0400

    Added some Arduino code
    It's independent from the rest of the program, but it backs up, in case
    my pc wants to go braindead again.

---
+++ MotorMicro/MotorMicro.ino
@@ -0,0 +1,46 @@
+String signal;
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    signal = Serial.readString();
+    //msg = signal;
+    while (true)
+    {
+      int startin = signal.indexOf('<');
+      if (startin < 0)
+      {
+        break;
+      }
+      int endin = signal.indexOf('>');
+      if (endin < 0)
+      {
+        break;
+      }
+      int msglngth = endin - startin;
+      String msg = signal.substring(startin + 1, endin + 1);
+      char key = msg[0];
+      int value = msg.substring(1).toInt();
+      //Serial.println(value);
+      signal = signal.substring(endin + 1);
+      //Serial.println(signal);
+
+      switch (key)
+      {
+        case 'C':
+          if (value == 0)
+          {
+            Serial.println("<C1>");
+          }
+      }
+    }
+    //msg = prevmsg;
+  }
+
+}
