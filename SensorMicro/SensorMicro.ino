d89c3b9b6de43d47d12154db15b98aa31fbce94f
cleclerc1 <cleclerc@ltu.edu>
2017-03-30 18:42:43 -0400

    Quick Fix for Efficiency
    Stopped mm being declared 3 times, set pin modes for Ultrasonic sensors
    in setup.

--- SensorMicro/SensorMicro.ino
+++ SensorMicro/SensorMicro.ino
@@ -2,6 +2,14 @@ String signal;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(5, INPUT);
+  pinMode(9, INPUT);
+  pinMode(13, INPUT);
   // put your setup code here, to run once:
 
 }
@@ -44,19 +52,19 @@ void loop() {
   }
   //String Path = Hall_Effect_Array() // insert stuff
   long Ultrasonic_1 = Ultra_Sensor(2, 3); //Call for First Ultrasonic
-  String Ultra_1 = "A";
+  String Ultra_1 = "V";
   Ultra_1 += String(Ultrasonic_1);
   long Ultrasonic_2 = Ultra_Sensor( 4, 5); //Call for Second Ultrasonic
-  String Ultra_2 = "B";
+  String Ultra_2 = "W";
   Ultra_2 += String(Ultrasonic_2);
-  long Ultrasonic_3 = Ultra_Sensor( 6, 7); //Call for First Ultrasonic
-  String Ultra_3 = "C";
+  long Ultrasonic_3 = Ultra_Sensor( 6, 7); //Call for Third Ultrasonic
+  String Ultra_3 = "X";
   Ultra_3 += String(Ultrasonic_3);
-  long Ultrasonic_4 = Ultra_Sensor( 8, 9); //Call for First Ultrasonic
-  String Ultra_4 = "D";
+  long Ultrasonic_4 = Ultra_Sensor( 8, 9); //Call for Fourth Ultrasonic
+  String Ultra_4 = "Y";
   Ultra_4 += String(Ultrasonic_4);
-  long Ultrasonic_5 = Ultra_Sensor( 12, 13); //Call for First Ultrasonic
-  String Ultra_5 = "E";
+  long Ultrasonic_5 = Ultra_Sensor( 12, 13); //Call for Fifth Ultrasonic
+  String Ultra_5 = "Z";
   Ultra_5 += String(Ultrasonic_5);
   
 }
